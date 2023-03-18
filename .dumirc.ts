01f0e0533e70e7b7cff618e4b2fbd3271436cd0f
xyy_521@163.com <xyy_5210@163.com>
2023-03-18 15:30:56 +0800

    feat: test

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'dumi';
-
+const repo = '13632632437';
 export default defineConfig({
   themeConfig: {
     name: 'git_home',
-    base: "/dist"
+    base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/'
   },
 });
